25fe14615d9321475d30dee8f842373acf62fb62
Gerald Versluis <gerald@verslu.is>
2020-08-27 12:51:49 +0200

    Initial commit

---
+++ BehaviorsSample/MainPageModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace BehaviorsSample
+{
+    public class MainPageModel : INotifyPropertyChanged
+    {
+        private int clickCount;
+
+        public int ClickCount
+        {
+            get => clickCount;
+            set
+            {
+                clickCount = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Command ClickCommand { get; set; }
+
+        public MainPageModel()
+        {
+            ClickCommand = new Command(() => ClickCount++);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
